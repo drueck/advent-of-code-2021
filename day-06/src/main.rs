42fa5b3a1295354219fd00cf0162b5a0f2947788
David Rueck <drueck@gmail.com>
2021-12-07 01:04:38 -0800

    Day 6 part 2: use a Vec instead of a HashMap

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -2,12 +2,12 @@
 // https://adventofcode.com/2021/day/5
 // Usage `cargo run <input-file> <days>
 
-use std::{collections::HashMap, env, fs::File, io::BufRead, io::BufReader};
+use std::{env, fs::File, io::BufRead, io::BufReader};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_file: &String = &args[1];
-    let days: usize = args[2].parse().expect("please specify the number of days");
+    let simulation_days: usize = args[2].parse().expect("please specify the number of days");
 
     let file = File::open(input_file).expect("no such file");
     let mut buf = BufReader::new(file);
@@ -22,25 +22,25 @@ fn main() {
         .map(|days| days.parse().expect("not an int"))
         .collect();
 
-    let mut timer_counts: HashMap<usize, usize> = HashMap::new();
+    let mut timer_counts: Vec<usize> = vec![0; 9];
+
     for days in lanternfish_timers {
-        *timer_counts.entry(days).or_insert(0) += 1;
+        timer_counts[days] += 1;
     }
 
-    for _day in 1..=days {
-        let spawners = timer_counts.get(&0).unwrap_or(&0).clone();
+    for _day in 1..=simulation_days {
+        let spawners = timer_counts[0];
         for days in 0..8 {
-            let next_day_count = timer_counts.get(&(days + 1)).unwrap_or(&0).clone();
-            *timer_counts.entry(days).or_insert(0) = next_day_count;
+            timer_counts[days] = timer_counts[days + 1];
         }
-        *timer_counts.entry(6).or_insert(0) += spawners;
-        *timer_counts.entry(8).or_insert(0) = spawners;
+        timer_counts[6] += spawners;
+        timer_counts[8] = spawners;
     }
 
-    let total_population: usize = timer_counts.values().sum();
+    let total_population: usize = timer_counts.iter().sum();
 
     println!(
         "The total population after {} days would be {}",
-        days, total_population
+        simulation_days, total_population
     );
 }
