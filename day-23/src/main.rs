6ce3351752dd337f8b64833910e44b1e6d9fab8f
David Rueck <drueck@gmail.com>
2022-01-08 01:14:11 -0800

    Day 23, Part 1: Store min energy for each state
    With this the test input took 8s to solve, and the real input took
    around 90s I think. Too long. The code is still super messy too.

--- day-23/src/main.rs
+++ day-23/src/main.rs
@@ -31,12 +31,15 @@
 // D => (9,0) or (9,1)
 
 use day_23::Burrow;
-use std::{collections::BinaryHeap, env, fs};
+use std::collections::{BinaryHeap, HashMap};
+use std::{env, fs};
 
 fn main() {
     let filename = env::args().nth(1).unwrap();
     let input = fs::read_to_string(filename).unwrap();
     let initial_burrow = Burrow::new(&input);
+
+    let mut min_energies: HashMap<Vec<((usize, usize), char)>, usize> = HashMap::new();
     let mut priority_queue: BinaryHeap<Burrow> = BinaryHeap::new();
     let mut min_energy = usize::MAX;
 
@@ -45,15 +48,20 @@ fn main() {
     priority_queue.push(initial_burrow);
 
     while let Some(burrow) = priority_queue.pop() {
-        // println!(
-        //     "states: {}, energy used for this state: {}",
-        //     priority_queue.len(),
-        //     burrow.energy_used
-        // );
-
         if burrow.energy_used > min_energy {
             break;
         }
+
+        let amphipod_locations: Vec<((usize, usize), char)> =
+            burrow.map.clone().into_iter().collect();
+
+        let min_energy_for_burrow = min_energies.entry(amphipod_locations).or_insert(usize::MAX);
+
+        if burrow.energy_used >= *min_energy_for_burrow {
+            continue;
+        }
+
+        *min_energy_for_burrow = burrow.energy_used;
 
         if burrow.organized() && burrow.energy_used < min_energy {
             min_energy = burrow.energy_used;
