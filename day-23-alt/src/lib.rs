6475df82f675e4591061f114a6fef01c8ff74675
David Rueck <drueck@gmail.com>
2022-01-15 23:21:24 -0800

    Day 23, Part 1: Can now apply all moves into home rooms
    burrow.move_into_rooms will keep checking for possible moves of
    amphipods into their home rooms until no such moves are available. from
    this point we can check to see if the burrow is organized, and if not
    perform all the possible moves into hallways, which could each have
    move_into_rooms applied to them before checking their organized state
    and pushing them onto the pq. Hopefully this process will greatly reduce
    the number of possible states to have to check.

--- day-23-alt/src/lib.rs
+++ day-23-alt/src/lib.rs
@@ -12,6 +12,8 @@ const HORIZONTAL_WITH_STEM: char = '╩';
 const BURROW_MAX_WIDTH: usize = 13;
 const BURROW_MIN_WIDTH: usize = 9;
 
+const HALLWAY_ROW: usize = 1;
+
 const A_ROOM_COL: usize = 3;
 const B_ROOM_COL: usize = 5;
 const C_ROOM_COL: usize = 7;
@@ -19,8 +21,37 @@ const D_ROOM_COL: usize = 9;
 
 const KINDS: [char; 4] = ['A', 'B', 'C', 'D'];
 
+fn energy_cost_for_kind(kind: &Kind) -> usize {
+    match kind {
+        &'A' => 1,
+        &'B' => 10,
+        &'C' => 100,
+        &'D' => 1000,
+        _ => panic!("invalid kind"),
+    }
+}
+
 pub type Position = (usize, usize);
 pub type Kind = char;
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Move {
+    pub from: Position,
+    pub to: Position,
+    pub kind: Kind,
+    pub cost: usize,
+}
+
+impl Move {
+    pub fn new(from: Position, to: Position, kind: Kind, cost: usize) -> Move {
+        Move {
+            from,
+            to,
+            kind,
+            cost,
+        }
+    }
+}
 
 pub struct Burrow {
     pub map: HashMap<Position, Kind>,
@@ -65,7 +96,7 @@ impl Burrow {
         }
     }
 
-    fn room_for(&self, kind: &Kind) -> impl Iterator<Item = Position> + '_ {
+    fn room_for(&self, kind: &Kind) -> Vec<Position> {
         let x = match kind {
             &'A' => A_ROOM_COL,
             &'B' => B_ROOM_COL,
@@ -74,17 +105,139 @@ impl Burrow {
             _ => panic!("Invalid amphipod species"),
         };
 
-        (2..(self.height - 2)).map(move |y| (x, y))
+        (2..(self.height - 1)).map(move |y| (x, y)).collect()
     }
 
     pub fn organized(&self) -> bool {
         KINDS.iter().all(|kind| {
             self.room_for(&kind)
+                .iter()
                 .all(|position| match self.map.get(&position) {
                     Some(kind_in_space) => kind_in_space == kind,
                     None => false,
                 })
         })
+    }
+
+    fn open_spaces_in_rooms(&self) -> Vec<(Position, Kind)> {
+        let mut open_spaces = vec![];
+
+        for kind in KINDS {
+            for position in self.room_for(&kind).into_iter().rev() {
+                match self.map.get(&position) {
+                    Some(kind_in_position) => {
+                        if kind_in_position != &kind {
+                            break;
+                        }
+                    }
+                    None => {
+                        open_spaces.push((position, kind));
+                        break;
+                    }
+                }
+            }
+        }
+
+        open_spaces
+    }
+
+    /// includes the target position but not the from position
+    fn positions_between(&self, from: &Position, to: &Position) -> Vec<Position> {
+        let mut spaces = vec![];
+
+        // move from a room to the hallway if needed
+        if from.1 > HALLWAY_ROW {
+            for y in HALLWAY_ROW..=(from.1 - 1) {
+                spaces.push((from.0, y));
+            }
+        }
+
+        // move left or right in the hallway
+        if to.0 < from.0 {
+            for x in to.0..=(from.0 - 1) {
+                spaces.push((x, HALLWAY_ROW));
+            }
+        } else {
+            for x in (from.0 + 1)..=to.0 {
+                spaces.push((x, HALLWAY_ROW));
+            }
+        }
+
+        // move from the hallway into a room if needed
+        if to.1 > HALLWAY_ROW {
+            for y in (HALLWAY_ROW + 1)..=to.1 {
+                spaces.push((to.0, y));
+            }
+        }
+
+        spaces
+    }
+
+    fn try_move(&self, from: Position, to: Position) -> Option<Move> {
+        let kind = self
+            .map
+            .get(&from)
+            .expect("no amphipod was found at the from location");
+        let positions = self.positions_between(&from, &to);
+
+        if positions
+            .iter()
+            .all(|position| self.map.get(position) == None)
+        {
+            return Some(Move::new(
+                from,
+                to,
+                *kind,
+                energy_cost_for_kind(&kind) * positions.len(),
+            ));
+        }
+
+        None
+    }
+
+    fn away_from_home_amphipod_positions(&self, kind: &Kind) -> Vec<Position> {
+        let room_for_kind = self.room_for(kind);
+
+        self.map
+            .iter()
+            .filter(|(_, kind_in_space)| *kind_in_space == kind)
+            .filter(|(position, _)| !room_for_kind.contains(&position))
+            .map(|(position, _)| position)
+            .cloned()
+            .collect()
+    }
+
+    fn next_move_into_room(&self) -> Option<Move> {
+        for (to, kind) in self.open_spaces_in_rooms() {
+            for from in self.away_from_home_amphipod_positions(&kind) {
+                if let Some(move_to_home) = self.try_move(from, to) {
+                    return Some(move_to_home);
+                }
+            }
+        }
+
+        None
+    }
+
+    pub fn apply(&mut self, possible_move: Move) {
+        assert!(self.map.get(&possible_move.from) == Some(&possible_move.kind));
+        assert!(self.map.get(&possible_move.to) == None);
+
+        self.map.remove(&possible_move.from);
+        self.map.insert(possible_move.to, possible_move.kind);
+        self.energy_used += possible_move.cost;
+
+        println!("burrow after applying move: {:?}", possible_move);
+        println!("{}", self);
+    }
+
+    // move all amphipods that can move into their home rooms into them
+    // until all possible moves into home rooms are exhausted
+    // the energy used should be updated accordingly
+    pub fn move_into_rooms(&mut self) {
+        while let Some(possible_move) = self.next_move_into_room() {
+            self.apply(possible_move)
+        }
     }
 }
 
@@ -256,4 +409,64 @@ fn test_organized_large_burrow() {
           #########";
     let burrow = Burrow::new(&organized);
     assert!(burrow.organized());
+}
+
+#[test]
+fn test_next_move_into_room_small_burrow() {
+    let input = "
+        #############
+        #B....A...D.#
+        ###.#C#.#.###
+          #A#B#D#C#
+          #########
+    ";
+
+    let burrow = Burrow::new(input);
+
+    assert_eq!(
+        burrow.next_move_into_room(),
+        Some(Move::new((6, 1), (3, 2), 'A', 4))
+    )
+}
+
+#[test]
+fn test_apply_move() {
+    let input = "
+        #############
+        #B....A...D.#
+        ###.#C#.#.###
+          #A#B#D#C#
+          #########
+    ";
+
+    let mut burrow = Burrow::new(input);
+    let move_a = burrow.try_move((6, 1), (3, 2)).unwrap();
+    let move_a_cost = move_a.cost;
+    burrow.apply(move_a);
+
+    assert_eq!(burrow.energy_used, move_a_cost);
+    assert_eq!(burrow.map.get(&(6, 1)), None);
+    assert_eq!(burrow.map.get(&(3, 2)), Some(&'A'));
+}
+
+#[test]
+fn test_move_into_rooms_small_burrow() {
+    let input = "
+        #############
+        #B....A...DC#
+        ###.#C#.#.###
+          #A#B#D#.#
+          #########
+    ";
+    let mut burrow = Burrow::new(input);
+
+    burrow.move_into_rooms();
+
+    // A:  4 *    1 =    4
+    // B:  5 *   10 =   50
+    // C: 10 *  100 = 1000
+    // D:  8 * 1000 = 8000
+
+    assert!(burrow.organized());
+    assert_eq!(burrow.energy_used, 4 + 50 + 1000 + 8000);
 }
