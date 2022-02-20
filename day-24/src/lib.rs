d527d460a0d8e561e70df3832049b8fed99a78f8
David Rueck <drueck@gmail.com>
2022-02-20 01:02:03 -0800

    Day 24, Part 2: Solved mostly by hand and with lots of hints

--- day-24/src/lib.rs
+++ day-24/src/lib.rs
@@ -1,259 +1,94 @@
-type Register = char;
-
-#[derive(Debug)]
-enum Instruction {
-    Inp(Register),
-    Add(Register, Register),
-    AddLiteral(Register, isize),
-    Mul(Register, Register),
-    MulLiteral(Register, isize),
-    Div(Register, Register),
-    DivLiteral(Register, isize),
-    Mod(Register, Register),
-    ModLiteral(Register, isize),
-    Eql(Register, Register),
-    EqlLiteral(Register, isize),
+pub struct MonadVariant {
+    variables: [[isize; 3]; 14],
 }
 
-impl Instruction {
-    pub fn new(instruction: &str) -> Self {
-        let mut parts = instruction.split(" ");
-        let fun = parts.next().unwrap();
+impl MonadVariant {
+    pub fn from_program(program: &str) -> Self {
+        let mut variables = [[0isize; 3]; 14];
+        let mut lines_iter = program.lines();
 
-        match fun {
-            "inp" => Instruction::Inp(parts.next().unwrap().chars().nth(0).unwrap()),
-            _ => {
-                let a = parts.next().unwrap().chars().nth(0).unwrap();
-                let b_str = parts.next().unwrap();
-                match b_str.parse::<isize>() {
-                    Ok(b) => match fun {
-                        "add" => Instruction::AddLiteral(a, b),
-                        "mul" => Instruction::MulLiteral(a, b),
-                        "div" => Instruction::DivLiteral(a, b),
-                        "mod" => Instruction::ModLiteral(a, b),
-                        "eql" => Instruction::EqlLiteral(a, b),
-                        _ => panic!("no such instruction on this machine"),
-                    },
-                    _ => {
-                        let b = b_str.chars().next().unwrap();
-                        match fun {
-                            "add" => Instruction::Add(a, b),
-                            "mul" => Instruction::Mul(a, b),
-                            "div" => Instruction::Div(a, b),
-                            "mod" => Instruction::Mod(a, b),
-                            "eql" => Instruction::Eql(a, b),
-                            _ => panic!("no such instruction on this machine"),
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+        for i in 0..14 as usize {
+            lines_iter.next();
+            lines_iter.next();
+            lines_iter.next();
+            lines_iter.next();
+            let a_instruction = lines_iter.next().unwrap();
+            let b_instruction = lines_iter.next().unwrap();
+            lines_iter.next();
+            lines_iter.next();
+            lines_iter.next();
+            lines_iter.next();
+            lines_iter.next();
+            lines_iter.next();
+            lines_iter.next();
+            lines_iter.next();
+            lines_iter.next();
+            let c_instruction = lines_iter.next().unwrap();
+            lines_iter.next();
+            lines_iter.next();
 
-pub struct ALU {
-    pub w: isize,
-    pub x: isize,
-    pub y: isize,
-    pub z: isize,
-}
-
-impl ALU {
-    pub fn new() -> Self {
-        ALU {
-            w: 0,
-            x: 0,
-            y: 0,
-            z: 0,
-        }
-    }
-
-    pub fn reset(&mut self) {
-        self.w = 0;
-        self.x = 0;
-        self.y = 0;
-        self.z = 0;
-    }
-
-    pub fn execute(&mut self, program: &str, inputs: &[isize]) -> Result<isize, &'static str> {
-        let mut inputs_iter = inputs.iter();
-
-        program
-            .trim()
-            .split("\n")
-            .map(|line| Instruction::new(line.trim()))
-            .try_for_each(|instruction| self.execute_instruction(instruction, &mut inputs_iter))?;
-
-        Ok(self.z)
-    }
-
-    fn read(&self, register: Register) -> isize {
-        match register {
-            'w' => self.w,
-            'x' => self.x,
-            'y' => self.y,
-            'z' => self.z,
-            _ => panic!("no such register in this machine"),
-        }
-    }
-
-    fn write(&mut self, register: Register, value: isize) {
-        match register {
-            'w' => {
-                self.w = value;
-            }
-            'x' => {
-                self.x = value;
-            }
-            'y' => {
-                self.y = value;
-            }
-            'z' => {
-                self.z = value;
-            }
-            _ => panic!("no such register in this machine"),
-        }
-    }
-
-    fn execute_instruction<'a, I>(
-        &mut self,
-        instruction: Instruction,
-        inputs_iter: &mut I,
-    ) -> Result<(), &'static str>
-    where
-        I: Iterator<Item = &'a isize>,
-    {
-        match instruction {
-            Instruction::Inp(register) => {
-                if let Some(value) = inputs_iter.next() {
-                    self.write(register, *value)
-                } else {
-                    return Err("Out of inputs");
-                }
-            }
-            Instruction::Add(register_a, register_b) => {
-                return self.execute_instruction(
-                    Instruction::AddLiteral(register_a, self.read(register_b)),
-                    inputs_iter,
-                )
-            }
-            Instruction::Mul(register_a, register_b) => {
-                return self.execute_instruction(
-                    Instruction::MulLiteral(register_a, self.read(register_b)),
-                    inputs_iter,
-                )
-            }
-            Instruction::Div(register_a, register_b) => {
-                return self.execute_instruction(
-                    Instruction::DivLiteral(register_a, self.read(register_b)),
-                    inputs_iter,
-                )
-            }
-            Instruction::Mod(register_a, register_b) => {
-                return self.execute_instruction(
-                    Instruction::ModLiteral(register_a, self.read(register_b)),
-                    inputs_iter,
-                )
-            }
-            Instruction::Eql(register_a, register_b) => {
-                return self.execute_instruction(
-                    Instruction::EqlLiteral(register_a, self.read(register_b)),
-                    inputs_iter,
-                )
-            }
-            Instruction::AddLiteral(register, value) => {
-                self.write(register, self.read(register) + value)
-            }
-            Instruction::MulLiteral(register, value) => {
-                self.write(register, self.read(register) * value)
-            }
-            Instruction::DivLiteral(register, value) => {
-                self.write(register, self.read(register) / value)
-            }
-            Instruction::ModLiteral(register, value) => {
-                self.write(register, self.read(register) % value)
-            }
-            Instruction::EqlLiteral(register, value) => {
-                self.write(register, if self.read(register) == value { 1 } else { 0 })
-            }
+            variables[i][0] = a_instruction.split(" ").nth(2).unwrap().parse().unwrap();
+            variables[i][1] = b_instruction.split(" ").nth(2).unwrap().parse().unwrap();
+            variables[i][2] = c_instruction.split(" ").nth(2).unwrap().parse().unwrap();
         }
 
-        Ok(())
+        Self { variables }
+    }
+
+    pub fn execute(&self, possible_model_number: &isize) -> isize {
+        let model = possible_model_number.to_string();
+        if model.contains("0") {
+            return 1;
+        }
+        let digits: Vec<isize> = model
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as isize)
+            .collect();
+
+        let mut z = 0;
+        for i in 0..14 {
+            let w = digits[i];
+
+            let test = (z % 26) + self.variables[i][1] != w;
+            z /= self.variables[i][0];
+
+            if test {
+                z = (z * 26) + w + self.variables[i][2];
+            };
+        }
+
+        z
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
-    fn test_negation_program() {
-        let mut alu = ALU::new();
+    fn test_validation_variables_from_program() {
+        let program = fs::read_to_string("input.txt").unwrap();
+        let monad = MonadVariant::from_program(&program);
 
-        let program = "
-            inp x
-            mul x -1
-        ";
-        let inputs = [5];
-        alu.execute(program, &inputs).unwrap();
-
-        assert_eq!(alu.x, -5);
-    }
-
-    #[test]
-    fn test_three_times_larger_program() {
-        let mut alu = ALU::new();
-
-        let program = "
-            inp z
-            inp x
-            mul z 3
-            eql z x
-        ";
-
-        let false_inputs = [1, 2];
-        alu.execute(program, &false_inputs).unwrap();
-        assert_eq!(alu.z, 0);
-
-        let true_inputs = [4, 12];
-        alu.execute(program, &true_inputs).unwrap();
-        assert_eq!(alu.z, 1);
-    }
-
-    #[test]
-    fn test_to_binary_program() {
-        let mut alu = ALU::new();
-
-        let program = "
-            inp w
-            add z w
-            mod z 2
-            div w 2
-            add y w
-            mod y 2
-            div w 2
-            add x w
-            mod x 2
-            div w 2
-            mod w 2
-        ";
-
-        let inputs = [5];
-        alu.execute(program, &inputs).unwrap();
-
-        assert_eq!(alu.z, 1);
-        assert_eq!(alu.y, 0);
-        assert_eq!(alu.x, 1);
-        assert_eq!(alu.w, 0);
-
-        let inputs = [15];
-
-        alu.reset();
-        alu.execute(program, &inputs).unwrap();
-
-        assert_eq!(alu.z, 1);
-        assert_eq!(alu.y, 1);
-        assert_eq!(alu.x, 1);
-        assert_eq!(alu.w, 1);
+        assert_eq!(
+            monad.variables,
+            [
+                [1, 12, 4],
+                [1, 15, 11],
+                [1, 11, 7],
+                [26, -14, 2],
+                [1, 12, 11],
+                [26, -10, 13],
+                [1, 11, 9],
+                [1, 13, 12],
+                [26, -7, 6],
+                [1, 10, 2],
+                [26, -2, 11],
+                [26, -1, 12],
+                [26, -4, 3],
+                [26, -12, 13]
+            ]
+        )
     }
 }
--- day-24/src/main.rs
+++ day-24/src/main.rs
@@ -1,23 +1,51 @@
-use day_24::ALU;
+// Advent of Code 2021: Day 24
+// https://adventofcode.com/2021/day/24
+// Usage `cargo run <input-file>
+
+// I ended up attempting to analyze the code by hand to come up with a more optimal algorithm, but
+// I was still stuck, so I looked for hints on reddit and came across an explaination of how the
+// program is pushing and popping base-26 numbers onto and off of a stack. Once I understood that
+// was how it was working, I used my analysis of the problem thus far (a spreadsheet with each
+// digit's processing code in a separate column so I could see the unique bits between each, and a
+// sort of manual decompilation of the program) and calculated the relationships between the digits
+// of a potential model number that would result in an empty stack at the end. I then deduced the
+// minimum and maximum model numbers from those relationships and tested them with my simplifed
+// version of the program, and they passed, so I tried them and they were correct.
+//
+// This was the description of the algorithm that I read that unlocked this manual solution. I
+// tried not to read it too carefully, but I don't know if I would've figured it out without this:
+//
+// https://github.com/dphilipson/advent-of-code-2021/blob/master/src/days/day24.rs
+//
+// In my first commit I had an implementation of the ALU which just executed the program, but of
+// course that is not really useful, unfortunately.
+//
+// Anyway, here are the rules I manually derived for my particular input
+//
+// D1  == D14 + 8
+// D2  == D13 - 7
+// D3  ==  D4 + 7
+// D5  ==  D6 - 1
+// D7  == D12 - 8
+// D8  ==  D9 - 5
+// D10 == D11
+
+use day_24::MonadVariant;
 use std::env;
 use std::fs;
 
 fn main() {
-    let filename = env::args().nth(1).expect("please supply an input filename");
-    let program = fs::read_to_string(filename).expect("failed to read program from input file");
+    let input_filename = env::args().nth(1).expect("please supply an input file");
+    let program = fs::read_to_string(input_filename).expect("failed to read input from file");
+    let monad = MonadVariant::from_program(&program);
 
-    let mut alu = ALU::new();
+    let maybe_max: isize = 92928914999991;
+    let maybe_min: isize = 91811211611981;
 
-    let model: Vec<isize> = "13579246899999"
-        .chars()
-        .map(|c| c.to_digit(10).unwrap() as isize)
-        .collect();
-
-    match alu.execute(&program, &model) {
-        Ok(result) => println!(
-            "The model number was {}",
-            if result == 0 { "valid" } else { "invalid" }
-        ),
-        Err(reason) => println!("Program failed: {}", reason),
+    if monad.execute(&maybe_max) == 0 {
+        println!("max: {}", maybe_max);
+    }
+    if monad.execute(&maybe_min) == 0 {
+        println!("min: {}", maybe_min);
     }
 }
